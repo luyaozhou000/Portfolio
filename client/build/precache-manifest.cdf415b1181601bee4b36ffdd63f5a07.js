798ad2553dd20f3b8cf5ec6ed2f30440c8083cc0
luyaozhou000 <luyaozhou@hotmail.com>
2019-09-25 22:25:02 -0400

    updated favicon

---
+++ client/build/precache-manifest.cdf415b1181601bee4b36ffdd63f5a07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4dc2d8867b2de993cc427c22caeb7b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81725eaf9fc54dc5d542",
+    "url": "/static/css/main.053d882a.chunk.css"
+  },
+  {
+    "revision": "f61011c8bcc1fde19b4d",
+    "url": "/static/js/2.56002de2.chunk.js"
+  },
+  {
+    "revision": "81725eaf9fc54dc5d542",
+    "url": "/static/js/main.826089a4.chunk.js"
+  },
+  {
+    "revision": "df35b418445d95524673",
+    "url": "/static/js/runtime~main.488beccc.js"
+  },
+  {
+    "revision": "0ebbb00b0824f031bdbfcb38364becb1",
+    "url": "/static/media/Jump.0ebbb00b.mp4"
+  },
+  {
+    "revision": "6e9308783991b966ed3dbba1a614ef88",
+    "url": "/static/media/Kuriero Esperanto Dika.6e930878.ttf"
+  },
+  {
+    "revision": "66a4640ab07139326551137ac5b126a6",
+    "url": "/static/media/hero1.66a4640a.jpg"
+  },
+  {
+    "revision": "c68eb7c2aed609bda59b23f2bc352b6c",
+    "url": "/static/media/yaya.c68eb7c2.jpg"
+  }
+]);
